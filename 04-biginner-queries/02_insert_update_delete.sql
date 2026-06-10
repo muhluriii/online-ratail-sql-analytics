da58f76eba9cdac6920f81c10b101699b25bb97b
muhluriii <107072642+muhluriii@users.noreply.github.com>
2026-06-10 13:10:08 +0200

    Create 02_insert_update_delete.sql

---
+++ 04-biginner-queries/02_insert_update_delete.sql
@@ -0,0 +1,16 @@
+-- insert
+
+insert into customers
+values
+(7,'Tsakani','Baloyi','tsakani@gmail.com','Durban'):
+
+--update
+
+update customers
+set city = 'polokwane'
+where customer_id =7;
+
+--delete
+
+delete from customers
+where customer_id = 7;
